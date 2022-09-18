037459895dd860f2a62d275363fb2784e8c30ded
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2022-09-18 17:48:20 +0200

    updated

--- js/task-10.js
+++ js/task-10.js
@@ -1,25 +1,37 @@
-
-
-const btnCreate = document.querySelector("button[data-create]")
-const btnDestroy = document.querySelector("button[data-destroy]")
-const boxesArr = document.querySelector('#boxes')
-const input = document.querySelector('input')
-
-let boxSize = 30
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function createBox(amount) {
-  amount = input.value;
+const input = document.querySelector("input");
+const btnCreate = document.querySelector(`button[data-create]`);
+const boxes = document.querySelector("#boxes");
+const btnDestroy = document.querySelector(`button[data-destroy]`);
 
-  for (let i = 0, i = amount, i++){
-  const box = boxesArr.createElement('div');
-  box.style.width = boxSize + 'px'
-    box.style.height = boxSize + 'px'
-  
-  }
-  boxSize = 
-  
+input.addEventListener("input", onNumberBox);
+btnCreate.addEventListener("click", () => createBoxes(amounts));
+btnDestroy.addEventListener("click", destroyBoxes);
+
+let amounts;
+
+
+function onNumberBox() {
+  amounts = Number(input.value);
+  return amounts;
 }
+let size = 30;
+const box = [];
+
+function createBoxes(amounts) {
+  for (let i = 0; i < amounts; i++) {
+    boxes.insertAdjacentHTML(
+      "beforeend",
+      `<div class="color" style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`
+    );
+    size = size + 10;
+  }
+  boxes.append(...box);
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
